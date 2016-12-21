936cb5eff5c6865fa95800f85e594a1fb7bb9674
CLINIC1 <CLINIC1@LPC07SZY2>
2016-12-21 13:36:31 -0800

    updated bat to only start one process

--- src/ripoc.bat
+++ src/ripoc.bat
@@ -1,18 +1,3 @@
 @ECHO OFF
-cd C:\Users\CLINIC1\workspace\WatchDir\src
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\daveoj &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\janifr &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\gerabo &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\jlomnitz &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\kathma3 &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\kevism2 &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\leahsm &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\nicoth &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\prakash &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\zaug &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\frankljg2 &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\gpbotelho &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\georgeclaire &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\ferreirapro &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\mikeam &
-start java  com.vsp.ripoc.WatchDir C:\EzCapture\prisam &
+cd C:\Users\CLINIC1\git\ripoc\src
+start java  com.vsp.ripoc.WatchDir -r C:\EzCapture &
